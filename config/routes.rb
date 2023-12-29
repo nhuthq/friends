9019cb782083776c3b480fc9e9bd68f29054e453
nhuthq <>
2023-12-29 14:25:14 +0700

    add gm devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_for :models
   resources :friends
-  # resources :home 
-  
+  # resources :home
+
   get 'home/about'
   root 'home#index'
-  
-  
+
+
 end
